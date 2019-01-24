493a0c2a45b01b481172be4f8ebc0ec99767138b
Davis <ldav7984@740s-pl-109.local>
2019-01-24 12:43:50 -0700

    Created controller folder and a controller file.

---
+++ CPlusSampleProject/Controller/Controller.cpp
@@ -0,0 +1,19 @@
+//
+//  Controller.cpp
+//  CPlusSampleProject
+//
+//  Created by Davis, Lauren on 1/24/19.
+//  Copyright © 2019 Davis, Lauren. All rights reserved.
+//
+
+#include "Controller.hpp"
+
+Controller :: Controller()
+{
+    
+}
+
+void Controller :: start()
+{
+    cout << "This is inside the start method yeeee" << endl;
+}
---
+++ CPlusSampleProject/Controller/Controller.hpp
@@ -0,0 +1,24 @@
+//
+//  Controller.hpp
+//  CPlusSampleProject
+//
+//  Created by Davis, Lauren on 1/24/19.
+//  Copyright © 2019 Davis, Lauren. All rights reserved.
+//
+
+#ifndef Controller_hpp
+#define Controller_hpp
+
+#include <iostream>
+using namespace std;
+
+class Controller
+{
+private:
+    int specialNumber;
+public:
+    Controller();
+    void start();
+};
+
+#endif /* Controller_hpp */
